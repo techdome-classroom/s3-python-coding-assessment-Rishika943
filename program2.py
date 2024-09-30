4b63328cea7d1401972ff04a8950f6b435e9b286
Rishika Vishwakarma <143267904+Rishika943@users.noreply.github.com>
2024-09-30 15:40:16 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -12,8 +12,8 @@ class Solution(object):
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         for i in range(len(s)):
-        # If the current value is smaller than the next value, subtract it
-        if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+            
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
             total -= roman_map[s[i]]
         else:
             # Otherwise, add the current value to the total
