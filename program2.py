9b9ee865c08655b71bca0e710abb4e57d53dc2d8
Rishika Vishwakarma <143267904+Rishika943@users.noreply.github.com>
2024-09-30 15:39:31 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -9,8 +9,7 @@ class Solution(object):
 
 class Solution(object):
     def romanToInt(s: str) -> int:
-    # Dictionary to store Roman numerals and their integer values
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     
     # Initialize the total value to 0
     total = 0
