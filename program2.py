ab408349f645849649a0dfa20f53a5d36be2e1a6
Rishika Vishwakarma <143267904+Rishika943@users.noreply.github.com>
2024-09-30 15:39:36 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,7 +11,7 @@ class Solution(object):
     def romanToInt(s: str) -> int:
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     
-    # Initialize the total value to 0
+   
     total = 0
     
     # Iterate through each character in the Roman numeral string
