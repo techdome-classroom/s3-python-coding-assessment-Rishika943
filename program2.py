40078d5f85d83e7c45a6036ea729a2c69c9a5f9d
Rishika Vishwakarma <143267904+Rishika943@users.noreply.github.com>
2024-09-30 21:22:40 +0530

    program2.py

--- program2.py
+++ program2.py
@@ -1,18 +1,25 @@
-class Solution(object):
-    def romanToInt(s: str) -> int:
-       roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        for i in range(len(s)):
+class Solution:
+    def isValid(s: str) -> bool:
+    
+    bracket_map = {')': '(', ']': '[', '}': '{'}
+    stack = []
+    
+   
+    for char in s:
+       
+        if char in bracket_map:
             
-            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
-                total -= roman_map[s[i]]
-            else:
-                total += roman_map[s[i]]
-                return total
-    print(romanToInt("III"))  
-    print(romanToInt("LVIII")) 
-    print(romanToInt("MCMXCIV"))
-    
-    
-  
+            top_element = stack.pop() if stack else '#'
+            
+            if bracket_map[char] != top_element:
+                return False
+        else:
+            
+            stack.append(char)
+    return not stack
+
+
+print(isValid("()"))      
+print(isValid("()[]{}"))  
+print(isValid("(]"))     
 
