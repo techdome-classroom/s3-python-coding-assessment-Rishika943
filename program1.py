8248df6e04bcf7464974dba6c3383acdbe6d22bf
Rishika Vishwakarma <143267904+Rishika943@users.noreply.github.com>
2024-09-30 15:35:33 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -12,8 +12,7 @@ class Solution:
         stack = []
         for char in s:
              if char in bracket_map:
-            
-            top_element = stack.pop() if stack else '#'
+                 top_element = stack.pop() if stack else '#'
             # Check if the popped element matches the corresponding opening bracket
             if bracket_map[char] != top_element:
                 return False
@@ -27,6 +26,8 @@ print(isValid("()"))      # Output: True
 print(isValid("()[]{}"))  # Output: True
 print(isValid("(]"))
     
+            
+            
 
     
           # Output: False
